bd6e09355966e2d909cdb28a440a7772ba759da3
TSL-RamKrishna <ram_krishna.shrestha@tsl.ac.uk>
2018-11-29 11:06:58 +0000

    script updated. script check if output file exists and newer than inputfile

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 source bowtie2-2.2.9
 
-projectdir=/tsl/scratch/shrestha/bozeng/project_forward_genetic_screening/
+projectdir=/tsl/scratch/shrestha/bozeng/project_forward_genetic_screening
 ref=/tsl/scratch/shrestha/bozeng/project_forward_genetic_screening/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa
-
+cdnaref=/tsl/scratch/shrestha/bozeng/project_forward_genetic_screening/reference/Magnaporthe_oryzae.MG8.cdna.all.fa
 
 function check_file_timestamp() {
 	if test ! -e $2
 	then
-        	return 1
+		return 1
 	else
-        	if test $1 -nt $2
+		if test $1 -nt $2
 		then
-                	return 1
-        	else
-                	return 0
+			return 1
+		else
+			return 0
 		fi
 	fi
 
@@ -39,11 +39,15 @@ for WT in WT_1_1.fq.gz,WT_1_2.fq.gz  WT_2_1.fq.gz,WT_2_2.fq.gz WT_3_1.fq.gz,WT_3
 	R1=$(echo $WT | awk -F "," '{print $1}')
 	R2=$(echo $WT | awk -F "," '{print $2}')
 	sample=$(echo $R1 | sed 's/_/ /g' | awk '{print $1"_"$2}')
-	check_file_timestamp ${projectdir}/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa.1.bt2 ${projectdir}/result/${sample}_bowtie_aligned.sorted.bam
+	check_file_timestamp ${projectdir}/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa.1.bt2 ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam
 	if test $? -eq 1; then
-		bash ${projectdir}/scripts/align_with_bowtie.sh $projectdir $ref /tsl/data/reads/ntalbot/uv_mutagenesis/guy11_mep2_gfp_control/rna_conidia_guy11_mep2_gfp_control/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/guy11_mep2_gfp_control/rna_conidia_guy11_mep2_gfp_control/raw/${R2} ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam $sample
+		sbatch --mem 20g -o ${projectdir}/log/${sample}_align.log --cpus 2 -J ${sample} --wrap "bash ${projectdir}/scripts/align_with_bowtie.sh $projectdir $ref /tsl/data/reads/ntalbot/uv_mutagenesis/guy11_mep2_gfp_control/rna_conidia_guy11_mep2_gfp_control/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/guy11_mep2_gfp_control/rna_conidia_guy11_mep2_gfp_control/raw/${R2} ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam $sample"
 	fi
 	echo "Sample $sample aligning completed"
+	check_file_timestamp $cdnaref ${projectdir}/results/tophat_${sample}/accepted_hits.bam
+	if test $? -eq 1; then
+		sbatch --mem 20g -o ${projectdir}/log/${sample}_cdna_align.log --cpus 2 -J ${sample}_cdna --wrap "bash ${projectdir}/scripts/run_tophat.sh ${projectdir} $cdnaref ${sample} /tsl/data/reads/ntalbot/uv_mutagenesis/guy11_mep2_gfp_control/rna_conidia_guy11_mep2_gfp_control/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/guy11_mep2_gfp_control/rna_conidia_guy11_mep2_gfp_control/raw/${R2}"
+	fi
 done
 
 # aligning KO samples
@@ -51,11 +55,16 @@ for KO in KO_1_1.fq.gz,KO_1_2.fq.gz  KO_2_1.fq.gz,KO_2_2.fq.gz  KO_3_1.fq.gz,KO_
 	R1=$(echo $KO | awk -F "," '{print $1}')
 	R2=$(echo $KO | awk -F "," '{print $2}')
 	sample=$(echo $R1 | sed 's/_/ /g' | awk '{print $1"_"$2}')
-	check_file_timestamp ${projectdir}/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa.1.bt2 ${projectdir}/result/${sample}_bowtie_aligned.sorted.bam
+	check_file_timestamp ${projectdir}/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa.1.bt2 ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam
 	if test $? -eq 1; then
-		bash ${projectdir}/scripts/align_with_bowtie.sh $projectdir $ref /tsl/data/reads/ntalbot/uv_mutagenesis/rgs1_ko/rgs1_ko_rna_conidia/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/rgs1_ko/rgs1_ko_rna_conidia/raw/${R2} ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam $sample
+		echo sbatch --mem 20g -o ${projectdir}/log/${sample}_align.log --cpus 2 -J ${sample} --wrap "bash ${projectdir}/scripts/align_with_bowtie.sh $projectdir $ref /tsl/data/reads/ntalbot/uv_mutagenesis/rgs1_ko/rgs1_ko_rna_conidia/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/rgs1_ko/rgs1_ko_rna_conidia/raw/${R2} ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam $sample"
+
 	fi
 	echo "Sample $sample aligning completed"
+	check_file_timestamp $cdnaref ${projectdir}/results/tophat_${sample}/accepted_hits.bam
+	if test $? -eq 1; then
+		sbatch --mem 20g -o ${projectdir}/log/${sample}_cdna_align.log --cpus 2 -J ${sample}_cdna --wrap  "bash ${projectdir}/scripts/run_tophat.sh ${projectdir} $cdnaref ${sample}  /tsl/data/reads/ntalbot/uv_mutagenesis/rgs1_ko/rgs1_ko_rna_conidia/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/rgs1_ko/rgs1_ko_rna_conidia/raw/${R2}"
+	fi
 done
 
 #aligning CERT7 samples
@@ -63,9 +72,13 @@ for cer7 in CER7_1_1.fq.gz,CER7_1_2.fq.gz  CER7_2_1.fq.gz,CER7_2_2.fq.gz  CER7_3
 	R1=$(echo $cer7 | awk -F "," '{print $1}')
 	R2=$(echo $cer7 | awk -F "," '{print $2}')
 	sample=$(echo $R1 | sed 's/_/ /g' | awk '{print $1"_"$2}')
-	check_file_timestamp ${projectdir}/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa.1.bt2 ${projectdir}/result/${sample}_bowtie_aligned.sorted.bam
+	check_file_timestamp ${projectdir}/reference/Magnaporthe_oryzae.MG8.dna.toplevel.fa.1.bt2 ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam
 	if test $? -eq 1; then
-		bash ${projectdir}/scripts/align_with_bowtie.sh $projectdir $ref /tsl/data/reads/ntalbot/uv_mutagenesis/cer7_genome/cer7_conidia/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/cer7_genome/cer7_conidia/raw/${R2} ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam $sample
+		echo sbatch --mem 20g -o ${projectdir}/log/${sample}_align.log --cpus 2 -J ${sample} --wrap "bash ${projectdir}/scripts/align_with_bowtie.sh $projectdir $ref /tsl/data/reads/ntalbot/uv_mutagenesis/cer7_genome/cer7_conidia/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/cer7_genome/cer7_conidia/raw/${R2} ${projectdir}/results/${sample}_bowtie_aligned.sorted.bam $sample"
 	fi
 	echo "Sample $sample aligning completed"
+	check_file_timestamp $cdnaref ${projectdir}/results/tophat_${sample}/accepted_hits.bam
+	if test $? -eq 1; then
+		sbatch --mem 20g -o ${projectdir}/log/${sample}_cdna_align.log --cpus 2 -J ${sample}_cdna --wrap "bash ${projectdir}/scripts/run_tophat.sh ${projectdir} $cdnaref ${sample} /tsl/data/reads/ntalbot/uv_mutagenesis/cer7_genome/cer7_conidia/raw/${R1} /tsl/data/reads/ntalbot/uv_mutagenesis/cer7_genome/cer7_conidia/raw/${R2}"
+	fi
 done
